c0b12e1e63224a5b965f868dde335492f6477775
Jonathon Terry <10217028+Maverick1872@users.noreply.github.com>
2026-03-14 15:54:03 -0700

    chore: add git hook scripts

---
+++ .githooks/common.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+# shellcheck disable=SC2034 # Explains why variables are unused for readability
+DEFAULT_BRANCH=develop
+
+# Colors
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+YELLOW='\033[1;33m'
+BLUE='\033[0;34m'
+BOLD='\033[1m'
+RESET='\033[0m'
+
+# Disable colors if terminal doesn't support them
+if [ ! -t 1 ]; then
+  RED='' GREEN='' YELLOW='' BLUE='' BOLD='' RESET=''
+fi
+
+info()    { printf "${BLUE}ℹ ${BOLD}%s${RESET}\n" "$1"; }
+success() { printf "${GREEN}✔ ${BOLD}%s${RESET}\n" "$1"; }
+warn()    { printf "${YELLOW}⚠ ${BOLD}%s${RESET}\n" "$1"; }
+error()   { printf "${RED}✖ ${BOLD}%s${RESET}\n" "$1"; }
+separator()   { printf "\n"; }
