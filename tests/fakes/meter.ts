fda4479c5d35874ecd55c633ae6bfd77b3eb63cb
Jonathon Terry <10217028+Maverick1872@users.noreply.github.com>
2026-03-15 13:45:46 -0700

    refactor: update fake meter to support aserting counter increment attributes

--- tests/fakes/meter.ts
+++ tests/fakes/meter.ts
@@ -1,16 +1,39 @@
+import assert from 'node:assert';
 import type { Counter, Meter } from '@opentelemetry/api';
 
+interface CounterState {
+  total: number;
+  operations: CounterOperation[];
+}
+
+interface CounterOperation {
+  value: number;
+  labels: Record<string, unknown>;
+}
+
 export interface FakeMeter extends Partial<Meter> {
-  counters: Record<string, number>;
+  counters: Record<string, CounterState>;
 }
 
 export const fakeMeter: FakeMeter = {
   counters: {},
   createCounter(name: string): Counter {
-    fakeMeter.counters[name] = 0;
+    fakeMeter.counters[name] = { total: 0, operations: [] };
     return {
-      add(value: number) {
-        fakeMeter.counters[name]! += value;
+      add(value: number, attributes?: Record<string, unknown>) {
+        assert(
+          fakeMeter.counters[name],
+          `Counter with name ${name} does not exist`,
+        );
+        const counterState = fakeMeter.counters[name];
+
+        counterState.total = counterState.total
+          ? value + counterState.total
+          : value;
+        counterState.operations.push({
+          value,
+          labels: attributes || {},
+        });
       },
     } as Counter;
   },
@@ -18,6 +41,6 @@ export const fakeMeter: FakeMeter = {
 
 export function resetMeterCounters() {
   for (const key in fakeMeter.counters) {
-    fakeMeter.counters[key] = 0;
+    fakeMeter.counters[key] = { total: 0, operations: [] };
   }
 }
--- tests/standard/tombstone.test.ts
+++ tests/standard/tombstone.test.ts
@@ -1,4 +1,7 @@
-/** biome-ignore-all lint/complexity/useLiteralKeys: Can't use literal keys when the key comes from an index signature */
+/**
+ * biome-ignore-all lint/complexity/useLiteralKeys: Can't use literal keys when the key comes from an index signature
+ * biome-ignore-all lint/style/noNonNullAssertion: Leveraging index signature which requires non-null assertions
+ */
 import type { Meter } from '@opentelemetry/api';
 import { beforeAll, beforeEach, describe, expect, test } from 'vitest';
 import { configureTombstone, Deprecate } from '../../src/standard/tombstone.js';
@@ -55,93 +58,99 @@ describe('Tombstone', () => {
     resetMeterCounters();
   });
 
-  test('Constructing the deprecated class should log a deprecation warning', () => {
-    new DeprecatedTestClass();
+  describe('Class Decoration', () => {
+    test('Constructing the deprecated class should log a deprecation warning', () => {
+      new DeprecatedTestClass();
 
-    expect(warnMessages).toHaveLength(1);
-    expect(warnMessages[0]).toStrictEqual({
-      args: ["Deprecated class of 'DeprecatedTestClass' was instantiated."],
+      expect(warnMessages).toHaveLength(1);
+      expect(warnMessages[0]).toStrictEqual({
+        args: ["Deprecated class of 'DeprecatedTestClass' was instantiated."],
+      });
+      expect(fakeMeter.counters['tombstones']!.total).toBe(1);
     });
-    expect(fakeMeter.counters['tombstones']).toBe(1);
-  });
 
-  test('Constructing a class that extends a deprecated class should log a deprecation warning', () => {
-    new ExtendedClass();
+    test('Constructing a class that extends a deprecated class should log a deprecation warning', () => {
+      new ExtendedClass();
 
-    expect(warnMessages).toHaveLength(1);
-    expect(warnMessages[0]).toStrictEqual({
-      args: [
-        'Subclass (ExtendedClass) of deprecated class (DeprecatedTestClass) was instantiated.',
-      ],
+      expect(warnMessages).toHaveLength(1);
+      expect(warnMessages[0]).toStrictEqual({
+        args: [
+          'Subclass (ExtendedClass) of deprecated class (DeprecatedTestClass) was instantiated.',
+        ],
+      });
+      expect(fakeMeter.counters['tombstones']!.total).toBe(1);
     });
-    expect(fakeMeter.counters['tombstones']).toBe(1);
   });
 
-  test('Invoking the deprecated method should log a deprecation warning', () => {
-    testClass.testMethod();
+  describe('Class Method Decoration', () => {
+    test('Invoking the deprecated method should log a deprecation warning', () => {
+      testClass.testMethod();
 
-    expect(warnMessages).toHaveLength(1);
-    expect(warnMessages[0]).toStrictEqual({
-      args: ["Deprecated method 'testMethod' was invoked"],
+      expect(warnMessages).toHaveLength(1);
+      expect(warnMessages[0]).toStrictEqual({
+        args: ["Deprecated method 'testMethod' was invoked"],
+      });
+      expect(fakeMeter.counters['tombstones']!.total).toBe(1);
     });
-    expect(fakeMeter.counters['tombstones']).toBe(1);
   });
 
-  test('Accessing the deprecated accessor property should log a deprecation warning', () => {
-    testClass.testProperty;
+  describe('Class Accessor Decoration', () => {
+    test('Accessing the deprecated accessor property should log a deprecation warning', () => {
+      testClass.testProperty;
 
-    expect(warnMessages).toHaveLength(1);
-    expect(warnMessages[0]).toStrictEqual({
-      args: ["Deprecated property 'testProperty' was accessed"],
+      expect(warnMessages).toHaveLength(1);
+      expect(warnMessages[0]).toStrictEqual({
+        args: ["Deprecated property 'testProperty' was accessed"],
+      });
+      expect(fakeMeter.counters['tombstones']!.total).toBe(1);
     });
-    expect(fakeMeter.counters['tombstones']).toBe(1);
-  });
 
-  test('Setting the deprecated accessor property should log a deprecation warning', () => {
-    testClass.testProperty = 'new value';
+    test('Setting the deprecated accessor property should log a deprecation warning', () => {
+      testClass.testProperty = 'new value';
 
-    expect(warnMessages).toHaveLength(1);
-    expect(warnMessages[0]).toStrictEqual({
-      args: ["Deprecated property 'testProperty' was set"],
+      expect(warnMessages).toHaveLength(1);
+      expect(warnMessages[0]).toStrictEqual({
+        args: ["Deprecated property 'testProperty' was set"],
+      });
+      expect(fakeMeter.counters['tombstones']!.total).toBe(1);
     });
-    expect(fakeMeter.counters['tombstones']).toBe(1);
-  });
 
-  test('Invoking the deprecated getter method should log a deprecation warning', () => {
-    expect(testClass.internalProperty === 'hard private');
-    expect(warnMessages).toHaveLength(1);
-    expect(warnMessages[0]).toStrictEqual({
-      args: ["Deprecated property 'internalProperty' was accessed"],
+    test('Invoking the deprecated getter method should log a deprecation warning', () => {
+      expect(testClass.internalProperty === 'hard private');
+      expect(warnMessages).toHaveLength(1);
+      expect(warnMessages[0]).toStrictEqual({
+        args: ["Deprecated property 'internalProperty' was accessed"],
+      });
+      expect(fakeMeter.counters['tombstones']!.total).toBe(1);
     });
-    expect(fakeMeter.counters['tombstones']).toBe(1);
-  });
 
-  test('Invoking the deprecated setter method should log a deprecation warning', () => {
-    testClass.internalProperty = 'new value';
+    test('Invoking the deprecated setter method should log a deprecation warning', () => {
+      testClass.internalProperty = 'new value';
 
-    expect(warnMessages).toHaveLength(1);
-    expect(warnMessages[0]).toStrictEqual({
-      args: ["Deprecated property 'internalProperty' was set"],
+      expect(warnMessages).toHaveLength(1);
+      expect(warnMessages[0]).toStrictEqual({
+        args: ["Deprecated property 'internalProperty' was set"],
+      });
+      expect(fakeMeter.counters['tombstones']!.total).toBe(1);
     });
-    expect(fakeMeter.counters['tombstones']).toBe(1);
-  });
 
-  test('Decorating a getter-only property should not incur a side-effect of introducing a setter', () => {
-    const updateReadOnlyValue = () => {
-      // @ts-expect-error Intentionally trying to set a value on a getter-only property
-      testClass.getterOnly = 'new value';
-    };
+    test('Decorating a getter-only property should not incur a side-effect of introducing a setter', () => {
+      const updateReadOnlyValue = () => {
+        // @ts-expect-error Intentionally trying to set a value on a getter-only property
+        testClass.getterOnly = 'new value';
+      };
 
-    expect(updateReadOnlyValue).toThrowErrorMatchingInlineSnapshot(
-      '[TypeError: Cannot set property getterOnly of #<TestClass> which has only a getter]',
-    );
-    expect(warnMessages).toHaveLength(0);
-    expect(fakeMeter.counters['tombstones']).toBe(0);
-  });
+      expect(updateReadOnlyValue).toThrowErrorMatchingInlineSnapshot(
+        '[TypeError: Cannot set property getterOnly of #<TestClass> which has only a getter]',
+      );
+      expect(warnMessages).toHaveLength(0);
+      expect(fakeMeter.counters['tombstones']!.total).toBe(0);
+    });
 
-  test('Decorating a setter-only property should not incur a side-effect of introducing a getter', () => {
-    expect(testClass.setterOnly).toBeUndefined();
-    expect(warnMessages).toHaveLength(0);
-    expect(fakeMeter.counters['tombstones']).toBe(0);
+    test('Decorating a setter-only property should not incur a side-effect of introducing a getter', () => {
+      expect(testClass.setterOnly).toBeUndefined();
+      expect(warnMessages).toHaveLength(0);
+      expect(fakeMeter.counters['tombstones']!.total).toBe(0);
+    });
   });
 });
